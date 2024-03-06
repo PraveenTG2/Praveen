96753b42c87e3c5b9877b2c3b8d2e7e46f598719
P H O E N I X <126892304+PraveenTG2@users.noreply.github.com>
2024-03-06 21:32:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
-import ResumeNew from "./components/Resume/ResumeNew";
 
 import {
   BrowserRouter as Router,
@@ -42,7 +41,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
-          <Route path="/resume" element={<ResumeNew />} /> 
           <Route path="*" element={<Navigate to="/"/>} />
         </Routes>
       </div>
