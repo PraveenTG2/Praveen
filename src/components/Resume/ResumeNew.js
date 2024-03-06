1b77b5aa3e7b7c1b1979de63e79c8e1960296c2b
P H O E N I X <126892304+PraveenTG2@users.noreply.github.com>
2024-03-06 21:48:36 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Praveenkumar_rusume.pdf";
+import pdf from "../../Assets/Praveenkumar_rusume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
